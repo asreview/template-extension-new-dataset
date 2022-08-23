727346ad3b2cf116224870f262d7dab60a7ee470
Jelle <j.j.teijema@gmail.com>
2022-08-23 14:23:18 +0200

    Move dataset creation to init

--- asreviewcontrib/dataset_name/your_dataset.py
+++ asreviewcontrib/dataset_name/your_dataset.py
@@ -12,36 +12,37 @@ class ExampleDatasetGroup(BaseDataGroup):
     group_id = "example_group"
     description = "Example dataset group"
 
-    ExampleDatasetLocal = BaseDataSet(
-            dataset_id="example_dataset_local",
-            filepath=str(Path(Path(__file__).parent,
-                              'data',
-                              'your_dataset.csv')),
-            title="Example dataset (local)",
-            description="This is an example dataset that is stored locally.",
-            authors='Teijema, J.J. (2022)',
-            topic='example datasets',
-            link='ASReview.ai',
-            reference=None,
-            img_url=None,
-            license='MIT',
-            year='2022'
-        )
-
-    ExampleDatasetRemote = BaseDataSet(
-            dataset_id="example_dataset_remote",
-            filepath='https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/datasets/van_de_Schoot_2017/output/van_de_Schoot_2017.csv',  # noqa
-            title="Example dataset (remote)",
-            description="This is an example dataset that is stored remotely.",
-            authors='Teijema, J.J. (2022)',
-            topic=None,
-            link=None,
-            reference=None,
-            img_url=None,
-            license=None,
-            year=None
-        )
-
     def __init__(self):
         """Initialize the dataset group."""
-        super().__init__(self.ExampleDatasetLocal, self.ExampleDatasetRemote)
+
+        ExampleDatasetLocal = BaseDataSet(
+                dataset_id="example_dataset_local",
+                filepath=str(Path(Path(__file__).parent,
+                                'data',
+                                'your_dataset.csv')),
+                title="Example dataset (local)",
+                description="This is an example dataset that is stored locally.",
+                authors='Teijema, J.J. (2022)',
+                topic='example datasets',
+                link='ASReview.ai',
+                reference=None,
+                img_url=None,
+                license='MIT',
+                year='2022'
+            )
+
+        ExampleDatasetRemote = BaseDataSet(
+                dataset_id="example_dataset_remote",
+                filepath='https://raw.githubusercontent.com/asreview/systematic-review-datasets/master/datasets/van_de_Schoot_2017/output/van_de_Schoot_2017.csv',  # noqa
+                title="Example dataset (remote)",
+                description="This is an example dataset that is stored remotely.",
+                authors='Teijema, J.J. (2022)',
+                topic=None,
+                link=None,
+                reference=None,
+                img_url=None,
+                license=None,
+                year=None
+            )
+
+        super().__init__(ExampleDatasetLocal, ExampleDatasetRemote)
